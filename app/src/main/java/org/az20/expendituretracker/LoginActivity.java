2285c25fba03aa69fba53b8d9a7a400aa934f907
AAK <aalorwu@gmail.com>
2018-08-06 00:00:58 +0300

    resolved merge conflicts

--- app/src/main/java/org/az20/expendituretracker/LoginActivity.java
+++ app/src/main/java/org/az20/expendituretracker/LoginActivity.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.az20.expendituretracker.database.User;
-import org.az20.expendituretracker.database.UserRepository;
 import org.az20.expendituretracker.viewmodel.UserViewModel;
 import org.az20.expendituretracker.helpers.PasswordHash;
 
